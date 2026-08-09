368e12140a8bc1359d9e12a02bc0bea7230597ab
Binay Paul <binay.paul.1993@gmail.com>
2026-08-09 17:12:07 +0530

    Initial commit

---
+++ src/main/java/com/binaypaul/learnspringbootreactive/LearnSpringBootReactiveApplication.java
@@ -0,0 +1,11 @@
+package com.binaypaul.learnspringbootreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootReactiveApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootReactiveApplication.class, args);
+	}
+}
---
+++ src/test/java/com/binaypaul/learnspringbootreactive/LearnSpringBootReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binaypaul.learnspringbootreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
